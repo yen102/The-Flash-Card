6f0adca24bd7f99c99e9c79d7811ae3a73a711d2
nthaiyen <yen.nth183862@sis.hust.edu.vn>
2022-01-16 13:20:55 +0700

    Update

--- frontend/src/component/homepage/categories/index.js
+++ frontend/src/component/homepage/categories/index.js
@@ -1,11 +1,15 @@
 import Category from "./Category";
 
 const Categories = (props) => {
-  return (
-    <div>
-      {props.categories.map((category, index) => <Category key={index} name={category.name} listDecks={category.listDecks} innerRef={category.innerRef} />)}
-    </div>
-  );
-}
+        return ( <
+                div > {
+                    props.categories.map((category, index) => < Category key = { index }
+                        name = { category.name }
+                        listDecks = { category.Decks }
+                        innerRef = { category.innerRef }
+                        />)} <
+                        /div>
+                    );
+                }
 
-export default Categories;
+                export default Categories;
--- frontend/src/component/homepage/index.js
+++ frontend/src/component/homepage/index.js
@@ -7,26 +7,30 @@ import { getAPI } from '../../api';
 
 const Homepage = () => {
 
-  const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    const getCategories = async () => {
-      const { listCategories } = await getAPI('/data/getDecks');
-      console.log(listCategories);
-      listCategories.map(category => ({...category, ref: React.createRef()}));
-      setCategories(listCategories);
-    }
+    useEffect(() => {
+        const getCategories = async() => {
+            const { listCategories } = await getAPI('/data/getDecks');
+            console.log(listCategories);
+            listCategories.map(category => ({...category, ref: React.createRef() }));
+            setCategories(listCategories);
+        }
 
-    getCategories();
-  }, []);
+        getCategories();
+    }, []);
 
-  return (
-    <div className='homepage'>
-      <HeaderHomepage />
-      <Bar categories={categories} />
-      <Categories categories={categories} />
-    </div>
-  );
+    return ( <
+        div className = 'homepage' >
+        <
+        HeaderHomepage / >
+        <
+        Bar categories = { categories }
+        /> <
+        Categories categories = { categories }
+        /> <
+        /div>
+    );
 }
 
 export default Homepage;
